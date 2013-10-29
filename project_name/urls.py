cd45e34afdcb89e8ba5617ebe0b58081b3543859
Jonathan Chu <jonathan.chu@me.com>
2013-10-29 00:52:25 -0400

    Reformat main project URLs file and add in an empty home template.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,11 +1,20 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
+
     # Examples:
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
     # url(r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
